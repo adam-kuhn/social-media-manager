36a9314b38e52b317f95f0f86fcfbcd75244b443
adam-kuhn <adam.kuhn20@gmail.com>
2019-12-21 17:21:36 +0200

    add basic Login and Logout feature to Facebook

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import './App.css'
 
-function App() {
+class App extends React.Component {
+  handleFacebookLogin = () => {
+    window.FB.login(function(response) {
+      if (response.authResponse) {
+       console.log('Welcome!  Fetching your information.... ')
+       window.FB.api('/me', function(response) {
+         console.log('Good to see you, ' + response.name + '.')
+       })
+      } else {
+       console.log('User cancelled login or did not fully authorize.')
+      }
+    }
+  )}
+
+  handleFacebookLogout = () => {
+    window.FB.getLoginStatus((response) => {
+      if (response.status === 'connected') {
+        console.log('conne')
+        window.FB.logout(function(response) {
+          console.log(response)
+        })
+      }
+    })
+  }
+
+render () {
   return (
     <div className="App">
       <header className="App-header">
       <h2>Social Media Manager</h2>
+      <button onClick={this.handleFacebookLogin}>Login</button>
+      <button onClick={this.handleFacebookLogout}>Logout</button>
       </header>
     </div>
   );
+}
 }
 
 export default App
