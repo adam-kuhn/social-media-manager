c17556e53738c5a77e620e4a735869cbf0e560de
adam-kuhn <adam.kuhn20@gmail.com>
2020-02-26 21:16:50 +0200

    convert to functional component, use useHistory to redirect on logout

--- src/components/FacebookAuth/FacebookLogout/FacebookLogout.jsx
+++ src/components/FacebookAuth/FacebookLogout/FacebookLogout.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom'
 
-class FacebookLogout extends React.Component {
-  handleFacebookLogout = () => {
+const FacebookLogout = (props) => {
+  const history = useHistory()
+  const handleFacebookLogout = () => {
     window.FB.getLoginStatus((response) => {
       if (response.status === 'connected') {
         window.FB.logout((response) => {
-          this.props.setUserLoggedInStatus(false)
+          props.setUserLoggedInStatus(false)
+          history.push('/')
         })
       }
     })
   }
-  render () {
-    return (
-      <button disabled={!this.props.userIsLoggedIn} onClick={this.handleFacebookLogout}>Logout</button>
-    )
+  // Used so I don't have to connect to Facebook SDK using ngrok
+  // for when I'm lazy and just testing non FB functionality
+  const developmentLogout = () => {
+    props.setUserLoggedInStatus(false)
+    history.push('/')
   }
+    return (
+      <button disabled={!props.userIsLoggedIn} onClick={developmentLogout}>Logout</button>
+    )
 }
 
 export default FacebookLogout
