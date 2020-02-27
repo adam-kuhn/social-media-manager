b44277ade80d241d26768093e06ddd9a4aea7b21
adam-kuhn <adam.kuhn20@gmail.com>
2020-02-27 20:06:05 +0200

    move dev logout into main logout handler

--- src/components/FacebookAuth/FacebookLogout/FacebookLogout.jsx
+++ src/components/FacebookAuth/FacebookLogout/FacebookLogout.jsx
@@ -4,6 +4,10 @@ import {useHistory} from 'react-router-dom'
 const FacebookLogout = (props) => {
   const history = useHistory()
   const handleFacebookLogout = () => {
+    // Used so I don't have to connect to Facebook SDK using ngrok
+    // for when I'm lazy and just testing non FB functionality
+    const devLogin = true
+    if (devLogin) history.push('/') 
     window.FB.getLoginStatus((response) => {
       if (response.status === 'connected') {
         window.FB.logout((response) => {
@@ -13,15 +17,9 @@ const FacebookLogout = (props) => {
       }
     })
   }
-  // Used so I don't have to connect to Facebook SDK using ngrok
-  // for when I'm lazy and just testing non FB functionality
-  const developmentLogout = () => {
-    props.setUserLoggedInStatus(false)
-    history.push('/')
-  }
-    return (
-      <button disabled={!props.userIsLoggedIn} onClick={developmentLogout}>Logout</button>
-    )
+  return (
+    <button onClick={handleFacebookLogout}>Logout</button>
+  )
 }
 
 export default FacebookLogout
