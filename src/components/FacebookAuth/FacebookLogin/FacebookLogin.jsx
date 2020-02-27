33cd358d969d93de9d757324a838c38cab735ccc
adam-kuhn <adam.kuhn20@gmail.com>
2020-02-27 19:24:46 +0200

    move dev login inside of main login function

--- src/components/FacebookAuth/FacebookLogin/FacebookLogin.jsx
+++ src/components/FacebookAuth/FacebookLogin/FacebookLogin.jsx
@@ -4,27 +4,27 @@ import {useHistory} from 'react-router-dom'
 const FacebookLogin = (props) => {
   const history = useHistory()
   const facebookLoginHandler = () => {
-    if (window.FB) {
-      window.FB.login(response => {
-        if (response.authResponse) {
-         window.FB.api('/me', response => { // remove response if not intending to us it
-           props.setUserLoggedInStatus(true)
-           history.push('/main')
-         })
-        } else {
-         console.log('User cancelled login or did not fully authorize.')
-        }
-      })
+    // Used so I don't have to connect to Facebook SDK using ngrok
+    // for when I'm lazy and just testing non FB functionality
+    const devLogin = true
+    if (devLogin) history.push('/main')
+    else {
+      if (window.FB) {
+        window.FB.login(response => {
+          if (response.authResponse) {
+           window.FB.api('/me', response => { // remove response if not intending to us it
+             props.setUserLoggedInStatus(true)
+             history.push('/main')
+           })
+          } else {
+           console.log('User cancelled login or did not fully authorize.')
+          }
+        })
+      }
     }
   }
-  // Used so I don't have to connect to Facebook SDK using ngrok
-  // for when I'm lazy and just testing non FB functionality
-  const developmentLogin = () => {
-    props.setUserLoggedInStatus(true)
-    history.push('/main')
-  }
     return (
-      <button disabled={props.userIsLoggedIn} onClick={developmentLogin}>Login to Facebook</button>
+      <button onClick={facebookLoginHandler}>Login to Facebook</button>
     )
 }
 
